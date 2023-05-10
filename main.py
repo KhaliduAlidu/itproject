c12f6d29a064695c68a5c51b0feb80f6e0837406
KhaliduAlidu <130634980+KhaliduAlidu@users.noreply.github.com>
2023-05-10 09:07:14 +0300

    Update main.py
    I moved the code from codeHS to Github, now we can both work on it, the next step is for us to change the code from free response to multiple choice so that it is easier for the user to answer the question.

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def question_1():
 def question_2():
     print("Who is the current head of our school?")
 def question_3():
-    print("")
+    print("Who originally founded the Gems corporation")
 def question_4():
-    print("")
+    print("In which city was the first Gems school built?")
 def question_5():
-    print("")
+    print("What does GEMS stand for?")
 def question_6():
-    print("")
+    print("How many different nationalities of students are there in GEMS")
 def question_7():
-    print("")
+    print("What curriculum does our school follow?")
 def question_8():
-    print("")
+    print("What is our schools mascot name?")
 def question_9():
-    print("")
+    print("Which street is our school on?")
 def question_10():
-    print("")
+    print("What does AAQ stand for?")
 
 if start == "yes":
     question_1()
@@ -41,7 +41,73 @@ if start == "yes":
             print("Incorrect, the current head of GEMS AAQ is Mark Lentz")
     
     next = input("type yes if you would like to continue the quiz: ")
-    if next == "yes"
+    if next == "yes":
         question_3()
         answer_here_3 = input("Enter Answer: ")
-        if answer_here_3 == ""
+        if answer_here_3 == "Sunny Varkey":
+            print("Correct")
+        else:
+            print("Incorrect, the original founder of the GEMS corporation is Sunny Varkey")
+
+    next = input("type yes if you would like to continue the quiz: ")
+    if next == "yes":
+        question_4()
+        answer_here_4 = input("Enter Answer: ")
+        if answer_here_4 == "Dubai":
+            print("Correct")
+        else:
+            print("Incorrect, the fist GEMS school was founded in Dubai")
+            
+     next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_5()
+         answer_here_5 = input("Enter Answer: ")
+         if answer_here_5 == "Global Education Management System":
+             print("Correct")
+         else:
+             print("Incorrect, GEMS stands for Global Education Management System")
+             
+    next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_6()
+         answer_here_6 = input("Enter Answer: ")
+         if answer_here_6 == "80+":
+             print("Correct")
+         else:
+             print("Incorrect, ")
+             
+    next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_7()
+         answer_here_7 = input("Enter Answer: ")
+         if answer_here_7 == "american":
+             print("Correct")
+         else:
+             print("Incorrect, ")
+             
+    next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_8()
+         answer_here_8 = input("Enter Answer: ")
+         if answer_here_8 == "raptors":
+             print("Correct")
+         else:
+             print("Incorrect, ")
+             
+    next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_9()
+         answer_here_9 = input("Enter Answer: ")
+         if answer_here_9 == "Mian street":
+             print("Correct")
+         else:
+             print("Incorrect, ")
+             
+    next = input("type yes if you would like to continue the quiz: ")
+     if next == "yes":
+         question_10()
+         answer_here_10 = input("Enter Answer: ")
+         if answer_here_10 == "American Academy Qatar":
+             print("Correct")
+         else:
+             print("Incorrect, ")
